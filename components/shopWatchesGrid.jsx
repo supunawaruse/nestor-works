c44a217d22595ef93ece88c741bd38a36d735ff2
supunawaruse <supunawa@gmail.com>
2025-04-02 20:40:38 +0530

    shop watch grid changed

--- components/shopWatchesGrid.jsx
+++ components/shopWatchesGrid.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import React from 'react'
 
 const ShopWatchesGrid = ({ watches }) => (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-2">
+    <div className="grid grid-cols-1 xxs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 gap-6 p-2">
         {watches.map(watch => (
             <Link href={`/shop/${watch.id}`} key={watch.id} className="group overflow-hidden transition-transform duration-300 hover:scale-105 hover:shadow-md">
                 <Image
@@ -14,9 +14,8 @@ const ShopWatchesGrid = ({ watches }) => (
                     className="w-full h-auto object-cover"
                 />
                 <div className="p-4 text-center">
-                    <h3 className="text-sm md:text-base text-secondary mb-1">{watch.name}</h3>
-                    <p className="text-sm md:text-base text-secondary mb-1">{watch.model}</p>
-                    <p className="text-sm md:text-base text-primary">${watch.price}</p>
+                    <h3 className="text-sm md:text-base font-semibold text-secondary mb-1">{watch.name}</h3>
+                    <p className="text-sm md:text-lg text-primary">{watch.model}</p>
                 </div>
             </Link>
         ))}
